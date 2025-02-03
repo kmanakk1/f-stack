52f44a49b188bdcc8dbd9015261bd385e8c29ba7
kclinx <kaesimanakkal@gmail.com>
2025-02-03 16:15:21 -0600

    run fstack as user

--- lib/ff_init.c
+++ lib/ff_init.c
@@ -44,6 +44,9 @@ ff_init(int argc, char * const argv[])
 
     //dpdk_argv[dpdk_argc++] = strdup("--vdev=net_tap,iface=dtap0,remote=vethp1");
 
+    // run as user
+    dpdk_argv[dpdk_argc++] = strdup("--in-memory");
+    
     printf("DPDK_argv: ");
     for(int x=0; x<dpdk_argc; x++) {
         printf(" %s ", dpdk_argv[x]);
